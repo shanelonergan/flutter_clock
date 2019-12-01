22cc43503b73ddf2b7f64fb9f81fcd3d6cf0b89d
shanelonergan <sptlonergan@gmail.com>
2019-12-01 13:00:25 -0500

    set preffered orientations

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,13 @@ import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeLeft
+  ]);
+  runApp(MyApp());
 }
 
